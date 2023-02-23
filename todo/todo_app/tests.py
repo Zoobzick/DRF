fe829a07f0ae5ebb8976696bcc82ebdffa4956d8
Troover <94111543+Zoobzick@users.noreply.github.com>
2023-02-23 18:45:48 +0300

    lesson_9 (#7)

--- todo/todo_app/tests.py
+++ todo/todo_app/tests.py
@@ -2,7 +2,7 @@ import json
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
-from mixer.backend.django import mixer
+from mixer.backend.django import Mixer
 from django.contrib.auth.models import User
 from .views import TodoModelViewSet, ProjectsModelViewSet
 from users.views import CustomUserModelViewSet
--- todo/users/views.py
+++ todo/users/views.py
@@ -1,9 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import CustomUser
-from .serializers import CustomUserModelSerializer
+from .serializers import CustomUserModelSerializer, CustomUserModelSerializerWithStatus
 
 
 class CustomUserModelViewSet(ModelViewSet):
-    queryset = CustomUser.objects.all()
+    queryset = CustomUser.objects.all().order_by("-is_superuser")
     serializer_class = CustomUserModelSerializer
     http_method_names = ['get', 'put']
+
+    def get_serializer_class(self):
+        if self.request.version == "0.1":
+            return CustomUserModelSerializerWithStatus
+        return CustomUserModelSerializer
